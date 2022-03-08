453228e3cce22d5b1d09e0b7f07a104c46d28e45
Andre Casaco <andrecasaco7@gmail.com>
2022-03-08 00:17:57 +0000

    for loop and hash set for twosum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -6,36 +6,37 @@ public class TwoSum {
         int[] values = {2, 7, 4, -2};
         int target = 2;
 
-
-        //quick approach use a nested for loop to check if the sum of two elements is equal to the target given
-        //big O(n^2)
-        twoSum1(values, target);
-
-        twoSum(values, target);
+        System.out.println(twoSum1(values, target));
         System.out.println(twoSum(values, target));
-
-        /*
-        Loop through every number in the array
-
-        if set contains the given target true is returned
-
-        otherwise add the number to the set
-
-        if the end of the array is reached, return false no number found
-         */
 
     }
 
-    private static void twoSum1(int[] values, int target) {
+    private static void test(int[] values, int target) {
         for (int i = 0; i < values.length; i++)
             for (int j = i + 1; j < values.length; j++)
                 if (values[i] + values[j] == target)
                     System.out.println("number " + values[i] + " at index " + i + " + "
                             + "number " + values[j] + " at index " + j + " = " + target);
     }
+    //quick approach use a nested for loop to check if the sum of two elements is equal to the target given
+    //big O(n^2)
+    static boolean twoSum1(int[] values, int target) {
+        for (int i = 0; i < values.length; i++)
+            for (int j = i + 1; j < values.length; j++)
+                if (values[i] + values[j] == target)
+                    return true;
+        return false;
+    }
 
-    
+    /*
+       Loop through every number in the array
 
+       if set contains the given target true is returned
+
+       otherwise add the number to the set
+
+       if the end of the array is reached, return false no number found
+        */
     static boolean twoSum(int[] values, int target) {
         Set<Integer> set = new HashSet<>();
 
